04a907428b12008174c27efc1f89e076019fd084
Megamexlevi2 <megalevi72737@gmail.com>
2026-05-20 14:52:52 -0300

    fix linksv3

---
+++ zig/src/test.cpp
@@ -0,0 +1,8 @@
+// I created this file simply because I wanted to, to show that the project uses C++. 
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
